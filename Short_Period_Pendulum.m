aaa069363dd4237dc0b212b93d9804405c69877d
Mfeuter Joseph Tachia <73492177+Tachia@users.noreply.github.com>
2024-03-23 03:49:35 +0300

    Add files via upload

---
+++ Short_Period_Pendulum.m
@@ -0,0 +1,50 @@
+﻿% Principles of Mathematical Modelling
+% Example 1: Modeling of Pendulum with Short Period of Time
+
+% Define Function
+function dydt = pend(t,y)
+    % Initial Conditions
+    w = 1;
+    dydt(1) = y(2);
+    dydt(2) = -w^2*y(1);
+end
+
+% Time Span
+tspan = [0 100];
+init = [0 1];
+
+% ODE45 Solver
+odeopt = odeset('RelTol',0.00001,'AbsTol',0.00001,'InitialStep',0.5,'MaxStep',0.5);
+[t,y] = ode45(@(t,y) pend(t,y),tspan,init, odeopt);
+
+% Visualization
+plot(t,y);
+title('Pendulum with Short Period');
+xlabel('Time');
+ylabel('Position');
+legend('y1(t)','y2(t)');
+
+hold on;
+
+% Alternative Method
+% Initial consition
+omega = 1;
+
+% ODE Function
+dy_dt = @(t,y)[y(2);...
+               -omega.^2*y(1)];
+
+% ODE45 Solver
+odeopt = odeset('RelTol',0.00001,'AbsTol',0.00001,'InitialStep',0.5,'MaxStep',0.5);
+[t,y] = ode45(dy_dt,[0 100],[0.0 1.0],odeopt);
+
+% Visualization of the models
+subplot(1,2,1);
+plot(t,y(:,1),'r-',t,y(:,2),'b-');
+xlabel('Time');
+legend('y1(t)','y2(t)');
+subplot(1,2,2);
+plot(y(:,1),y(:,2),'b-');
+axis([-1.5 1.5 -1.5 1.5]);
+xlabel('Position (y2)');
+ylabel('Position (y1)');
---
+++ Undamped_pendulum.m
@@ -0,0 +1,43 @@
+﻿% ode45 - Undamped Pendulum
+% d^2theta/dt^2 = -omega^2sin(theta)
+
+% Parameters
+init = [0.0 1.0];
+function dxdt = eqn(t,y);
+   omega = 10;
+   dxdt(1) = y(2);
+   dxdt(2) = -omega.^2*sin(y(1));
+end
+
+% Time Span for the simulation
+tspan = [0 25];
+
+% ODE45 Function
+% odeopt = odeset('RelTol', 0.00001, 'AbsTol', 0.00001, 'InitialStep',0.5,'MaxStep',0.5);
+[t,y] = ode45(@(t,y) eqn(t,y), tspan, init);
+
+% Visualization of the models
+subplot(1,2,1);plot(t,y(:,1),'r-',t,y(:,2),'b-');xlabel('time'); legend('y1(t)','y2(t)');
+subplot(1,2,2);plot(y(:,1),y(:,2),'b-'); axis([-2.5 2.5 -2.5 2.5]); xlabel('y(2)');ylabel('y(1)');
+
+% Alternative 1
+omega = 10;
+tspan = [0.0 1.0];
+odeFun = @(t,y)[y(2); -omega.^2*sin(y(1))];
+odeopt = odeset('RelTol', 0.00001, 'AbsTol', 0.00001, 'InitialStep',0.5,'MaxStep',0.5);
+[t,y] = ode45(odeFun,[0 25], tspan, odeopt);
+
+% Visualization of the models
+subplot(1,2,1);plot(t,y(:,1),'r-',t,y(:,2),'b-');xlabel('time'); legend('y1(t)','y2(t)');
+subplot(1,2,2);plot(y(:,1),y(:,2),'b-'); axis([-2.5 2.5 -2.5 2.5]); xlabel('y(2)');ylabel('y(1)');
+
+% Alternative 2
+omega = 10;
+dy_dt = @(t,y)[y(2);...
+              -omega.^2*sin(y(1))];
+odeopt = odeset('RelTol',0.00001,'AbsTol',0.00001,'InitialStep',0.5,'MaxStep',0.5);
+[t,y] = ode45(dy_dt,[0 25],[0.0 1.7],odeopt);
+
+% Visualization of the models
+subplot(1,2,1);plot(t,y(:,1),'r-',t,y(:,2),'b-');xlabel('time'); legend('y1(t)','y2(t)');
+subplot(1,2,2);plot(y(:,1),y(:,2),'b-'); axis([-2.5 2.5 -2.5 2.5]); xlabel('y(2)');ylabel('y(1)');
